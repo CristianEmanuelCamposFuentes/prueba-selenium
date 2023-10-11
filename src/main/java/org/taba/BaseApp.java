819f1623ea4462d84c6d8212cf930bfef3594e1e
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-10-11 00:28:15 -0300

    Comenzando reestructuracion de proyecto

--- src/main/java/org/taba/BaseApp.java
+++ src/main/java/org/taba/BaseApp.java
@@ -2,9 +2,50 @@ package org.taba;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BaseApp {
-    protected WebDriver driver;
+    // El WebDriver es la instancia del navegador donde usamos Selenium
+    // Objeto estatico para el driver
+    protected static WebDriver driver;
+    // Objeto estatico para el wait que gestiona las esperas
+    protected static WebDriverWait wait;
+    // El objeto Actions realiza las acciones de interaccion del teclado y el mouse
+    protected static Actions actions;
+
+    // Bloque estatico para el drive para todas las instancias futuras de otras clases de pagina
+    // Le agrego una espera de 10seg
+    static {
+        // A futuro va a servir para personalizar configuraciones de datos(perfil, extensiones, etc)
+        ChromeOptions chromeOptions = new ChromeOptions();
+        // Setear navegador
+        driver = new ChromeDriver(chromeOptions);
+        // Setear espera
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Setear acciones
+        actions = new Actions(driver);
+
+    }
+
+    public BaseApp(WebDriver driver) {
+        BaseApp.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public static void navigateTo(String url){
+        // get() carga una pagina web nueva en la ventana del navegador actual
+        driver.get(url);
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
 
     public void initializeDriver() {
         // Agregar a la propiedad de sistema la ruta al
--- src/test/java/org/taba/AppTest.java
+++ src/test/java/org/taba/AppTest.java
@@ -1,7 +1,5 @@
 package org.taba;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
 
     @org.junit.jupiter.api.BeforeEach
