6364ba50f73fd52449dc3e14c61dc521b639b65e
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-10-10 21:46:04 -0300

    Adaptando clase Base

--- src/main/java/org/taba/BaseApp.java
+++ src/main/java/org/taba/BaseApp.java
@@ -3,25 +3,21 @@ package org.taba;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+public class BaseApp {
+    protected WebDriver driver;
 
-public class App {
-    public static void main(String[] args) throws InterruptedException {
+    public void initializeDriver() {
         // Agregar a la propiedad de sistema la ruta al
         // ejecutable de chromedriver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\crist\\IdeaProjects\\PruebaSelenium\\src\\main\\resources\\drivers\\chromedriver.exe");
-
         // Inicializar el navegador
-        WebDriver driver = new ChromeDriver();
-
-        // Abrir pagina de Google
-        driver.get("https://www.google.com");
-
-        // Maximizar el navegador
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
 
-        Thread.sleep(2000);
-        // Cerrar el navegador
-        driver.close();
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
