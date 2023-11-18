888d485b48e624a88521e626a5860ab3f6a0e6bd
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-17 22:27:46 -0300

    Setear inicio

---
+++ src/test/pages/HomePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+    @FindBy(id = "searchInput")
+    private WebElement searchInput;
+
+//    @FindBy(id = "searchInput")
+//    private WebElement searchInput;
+//
+//    @FindBy(id = "searchInput")
+//    private WebElement searchInput;
+
+}
